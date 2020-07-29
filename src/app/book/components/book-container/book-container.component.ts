00452c44e11f85ad39b537d6b6c02582328c7383
Kevin Urgilés <kevinurgiles1996@gmail.com>
2020-07-29 14:40:47 -0500

    Last changes

--- src/app/book/components/book-container/book-container.component.ts
+++ src/app/book/components/book-container/book-container.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from './../../../core/services/book.service';
 import { Book } from './../../../core/models/book.model';
 
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-book-container',
   templateUrl: './book-container.component.html',
@@ -10,39 +12,49 @@ import { Book } from './../../../core/models/book.model';
 })
 export class BookContainerComponent implements OnInit {
   books: Book[];
+  isLoading = false;
 
   constructor(private bookService: BookService) {}
 
   ngOnInit(): void {}
 
   fetchBooks(text: string) {
-    this.bookService.getBooks(text).subscribe((data) => {
-      const items = data.items;
-      this.books = items.map((item) => {
-        const { id } = item;
-        const {
-          title,
-          subtitle,
-          publisher,
-          publishedDate,
-          description,
-          authors,
-          categories,
-          imageLinks,
-        } = item.volumeInfo;
-        return {
-          id,
-          title,
-          subtitle,
-          publisher,
-          publishedDate,
-          description,
-          authors,
-          categories,
-          imageLinks,
-        };
+    this.books = [];
+    this.isLoading = true;
+    this.bookService
+      .getBooks(text)
+      .pipe(
+        map((data: any) => {
+          return [...data.items];
+        })
+      )
+      .subscribe((items) => {
+        this.books = items.map((item) => {
+          const { id } = item;
+          const {
+            title,
+            subtitle,
+            publisher,
+            publishedDate,
+            description,
+            authors,
+            categories,
+            imageLinks,
+          } = item.volumeInfo;
+          return {
+            id,
+            title,
+            subtitle,
+            publisher,
+            publishedDate,
+            description,
+            authors,
+            categories,
+            imageLinks,
+          };
+        });
+        this.isLoading = false;
       });
-    });
   }
 
   clearResults(clear: boolean) {
